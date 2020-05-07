76d835e487a23bcaf0d892faa72d23613db15828
hndsfohnd <g532848_nj@yahoo.co.jp>
2020-05-07 23:28:02 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'runaways#index'
+  resources :runaways, only: [:index] do
+    collection do
+      get 'difficult1'   
+      get 'difficult2' 
+    end 
+  end
 end
