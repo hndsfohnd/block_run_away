8d4c36c455731c883caff92726dfd2be28df8913
hndsfohnd <g532848_nj@yahoo.co.jp>
2021-02-16 23:10:45 +0900

    transaction create

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -5,6 +5,24 @@ class BlocksController < ApplicationController
   end
 
   def create
+    @block = Block.new(block_params)
+    if @block.save
+      create_hash(@block)
+      redirect_to blocks_path
+    else
+      render blocks_path
 
+    end
   end
+
+  private
+    def block_params
+      params.require(:block).permit(:transactiondata)
+    end    
+
+    def create_hash(block)
+      block.beforehash =  SecureRandom.hex(32)
+      block.save
+    end
+
 end
