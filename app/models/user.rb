cf04b0a77bc0fba43b89b1c040b8e2f0b856915b
hndsfohnd <g532848_nj@yahoo.co.jp>
2021-03-03 23:27:15 +0900

    assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy 
   has_many :following, through: :active_relationships
 
+  has_many :possive_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+
 end
